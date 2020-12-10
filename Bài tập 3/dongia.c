ae669707755e322ccba258883316947dece8ec0d
mkhanh1 <75061197+mkhanh1@users.noreply.github.com>
2020-12-10 16:56:20 +0700

    Create dongia.c

---
+++ Bài tập 3/dongia.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+	// khai bao 2 bien DG va SL
+  int DG va SL ;
+  double TT;
+  if ((Sl>5)=-25%){
+    // hien thi ra man hinh bien don gia 
+    printf (" DG = ");
+    scanf ("%d", &DG);
+    // hien thi ra man hinh bien so luong
+    printf ("SL = ");
+    scanf ("%d", &SL);
+   } else (SL
+    }
+    double TT,dongia,soluong ;
+    // so luong mua tu 5 tro len
+    printf ("%d", Sl = 5); 
+    TT = (SL + DG )/100*25;
+    // tinh so luong thanh tien mua  
+    printf ("(SL + DG0/100*25 =%d, DG,SL,TT"); 
+  getchar();
+  return 0;
+}
