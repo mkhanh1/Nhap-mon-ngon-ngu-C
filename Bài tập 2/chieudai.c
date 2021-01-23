b73ef4ec5374d80030ac7c808ec28c28226129bc
mkhanh1 <75061197+mkhanh1@users.noreply.github.com>
2021-01-23 07:15:51 +0700

    Create chieudai.c

---
+++ Bài tập 2/chieudai.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+  // khai bao bien va nhap du lieu tu ban phim
+  double l;
+  
+  
+  // khai bao don vi chieu dai m, cm, mm
+  double m, cm, mm;
+  m=l*1;
+  cm=l*100;
+  mm=l*1000;
+  
+ 
+  
+  printf("nhap l = ");// nhap l tu ban phim
+  scanf("%lf",&l);
+  printf("Met(m)\t Centimet(cm)\t Milimet(mm)\t  \n ");
+  printf("%.lf\t      %.lf\t         %.lf\t \n", l*1, l*100,l*1000  );
+   
+  getchar();
+  return 0;
+}
+© 2021 GitHub, Inc.
