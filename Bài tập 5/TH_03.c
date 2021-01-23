c0af157a3f1a45126830f94c37563c6c0ad17e88
mkhanh1 <75061197+mkhanh1@users.noreply.github.com>
2021-01-23 09:23:51 +0700

    Create TH_03.c

---
+++ Bài tập 5/TH_03.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main(){
+    int a, b, c;
+    printf("\nNhap so a: ");
+    scanf("%d", &a);
+    printf ("Nhap so b: ");
+    scanf ("%d",&b);
+    printf ("Nhap so C: ");
+    scanf ("%d",&c);
+    if (a >= b && a >= c){
+         printf("So lon nhat trong 3 so la:\na = %d", a);
+}else if (b >= a && b >= c){
+         printf("So lon nhat trong 3 so la:\nb = %d", b);
+}else {
+         printf("So lon nhat trong 3 so la:\nc = %d", c);
+     }
+ 
+}
