206674e6f85632d45e4d97cdb365c229dce85b1f
mkhanh1 <75061197+mkhanh1@users.noreply.github.com>
2021-01-23 10:12:05 +0700

    Update TH_03.c

--- Bài tập 5/TH_03.c
+++ Bài tập 5/TH_03.c
@@ -1,19 +1,30 @@
-#include <stdio.h>
- 
+//Khoi khai bao
+#include<stdio.h>
+int ba_so(int x , int y, int z);
+
+ //khoi ham main
 int main(){
-    int a, b, c;
-    printf("\nNhap so a: ");
-    scanf("%d", &a);
-    printf ("Nhap so b: ");
-    scanf ("%d",&b);
-    printf ("Nhap so C: ");
-    scanf ("%d",&c);
-    if (a >= b && a >= c){
-         printf("So lon nhat trong 3 so la %d", a);
-}else if (b >= a && b >= c){
-         printf("So lon nhat trong 3 so la %d", b);
-}else {
-         printf("So lon nhat trong 3 so la %d", c);
-     }
- 
+  int a,b,c;
+  printf("Nhap so a = ");
+  scanf("%d", &a);
+  printf("Nhap so b = ");
+  scanf("%d", &b);
+  printf("Nhap so c = ");
+  scanf("%d", &c);
+  ba_so(a,b,c);
+  return 0;
+}
+
+// khoi dinh nghia ham
+int ba_so(int x,int y,int z){
+  if(a>b   &&   a>c){
+   printf("so lon nhat la :%d",a);
+  }  
+ if (b>a   &&  b>c){
+ printf("so lon nhat la :%d",b);
+ }
+ if (c>a    &&c  >b){
+ printf("so lon nhat la :%d",c);
+ }
+ return 1;
 }
