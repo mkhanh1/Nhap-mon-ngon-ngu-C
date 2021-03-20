a6eaba7a2488739e7d4e80f16c229ff327cb52a3
mkhanh1 <75061197+mkhanh1@users.noreply.github.com>
2021-03-20 14:38:52 +0700

    Create phan1kienthuc.c

---
+++ phan1kienthuc.c
@@ -0,0 +1,8 @@
+phần 1 
+khái niệm biến là vùng bộ nhớ được cấp phát để luwu giá trị cho một kiểu giá trị cho một kiểu dữ liệu nào đó tại một thời điểm nhất định và nó được truy xuất thông qua một tteen đã được khái báo
+cho biến đó.
+cú pháp khái báo biến Kiểu_dữ liệu Tên_biến
+trong đó kiểu_dữ liệu xác định của dữ liệu và tên biến đó Tên_biến là một định danh được gán cho những vùng nhớ chứa biến và đừng để truy xuất giá trị của biến 
+các laoij ngôn ngữ C
+biến taonf bộ nếu một biến khái báo bên ngoài khối lệnh 
+biến cục bộ là biến được khai báo bên trong khối lện
